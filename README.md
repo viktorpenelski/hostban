b65f3e6f5b7683c3d47606c22f2d998f37dfe62d
Victor Penelski <victor.penelski@gmail.com>
2020-07-18 17:02:12 +0300

    bundle as 'uberJar' and add build/run instrucitons

--- README.md
+++ README.md
@@ -5,6 +5,16 @@
  This is achieved by adding a record for provided hosts in the local `hosts` file, alongside
  the IP `127.0.0.1`, effectively redirecting "banned" hosts to localhost, rendering them unreachable.
  
+ ---
+ 
+ ### Build:
+ `./gradlew clean install`
+ 
+ ### Run (with admin privilidges):
+ `java -jar build/libs/hostban*.jar`
+ 
+ ---
+ 
  The MVP version takes the list of hosts to be "banned" as a list in `Main.kt` 
  and supports only Windows.
  
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "1.3.72"
+    application
+    id("com.github.johnrengelman.shadow") version "5.0.0"
 }
 
-group = "org.example"
+group = "com.github.viktorpenelski"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -14,11 +18,24 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
 }
 
+application {
+    mainClassName = "com.github.viktorpenelski.hostban.MainKt"
+}
+
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+    shadowJar {
+        mergeServiceFiles()
+    }
+    build {
+        dependsOn(shadowJar)
+    }
+    jar {
+        enabled = false
     }
 }
